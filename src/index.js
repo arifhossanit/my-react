1a8275e0d0a0e8c2194f14d265fad5ca26735b6c
arifhossanit <arif94hossan@gmail.com>
2022-02-09 12:43:27 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const element =React.createElement('h1',{className:'heading'},'Hello, world');
+const element=(
+<h1 className='heading'>
+  Hello Worlds
+</h1>
+  );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello, {user}!</h1>;
+  }
+  return <h1>Hello, stranger.</h1>;
+}
+const element2= <h1>Hello, {getGreeting('arif')}</h1>
+console.log(element2);
+reactDom.render(element, document.getElementById('root'));
