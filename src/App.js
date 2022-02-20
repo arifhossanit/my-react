20e88883d29ea57ab1c37d8f2b14508d01b90382
arifhossanit <arif94hossan@gmail.com>
2022-02-20 12:53:56 +0600

    Render Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-import ClickCounter from './components/clickcounter/ClickCounter';
-import HoverCounter from './components/clickcounter/HoverCounter';
 import ClockList from './components/ClockList';
 import Bracket from './components/composition/Bracket';
 import Emoji from './components/composition/Emoji';
 import Text from './components/composition/Text';
 import Form from './components/Form';
+import ClickCounter from './components/hoc/ClickCounter';
+import HoverCounter from './components/hoc/HoverCounter';
+import ClickCounter2 from './components/renderprops/ClickCounter';
+import Counter from './components/renderprops/Counter';
+import HoverCounter2 from './components/renderprops/HoverCounter';
 // import Text from './components/inheritance/Text';
 import Calculator from './components/TemCal/Calculator';
 
@@ -26,6 +29,16 @@ function App() {
          {/* <Text/> */}
          <ClickCounter/>
          <HoverCounter/>
+            <Counter>
+                {(count,incrementCount)=>(
+                    <ClickCounter2 count={count} incrementCount={incrementCount}/>
+                    )}
+            </Counter>
+            <Counter>
+                {(count,incrementCount)=>(
+                    <HoverCounter2 count={count} incrementCount={incrementCount}/>
+                    )}
+            </Counter>
         </>
     );
 }
---
+++ src/components/renderprops/ClickCounter.js
@@ -0,0 +1,11 @@
+
+export function ClickCounter2({count, incrementCount}) {
+ 
+    return (
+      <div>
+          <button type='button' onClick={incrementCount}>Clicked {count} times</button>
+      </div>
+    )
+}
+
+export default ClickCounter2
